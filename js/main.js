3536540941d4b92010b815fb2cac4b79961f0a94
Eduardo Geek <eduardo.geek@gmail.com>
2020-02-20 00:40:11 -0300

    Add "Courses, Events and Workshop" and fix CSS - img background

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*  Load file external to table */
+$(document).ready(function() {
+  $('#table-courses').DataTable( {
+    "ajax": 'ajax/data/courses.txt',
+    "searching": true,
+    "info": true,
+    "pagingType": "full_numbers",
+    "order": [[ 2, 'desc' ], [ 0, 'asc' ]]
+  } );
+} );
